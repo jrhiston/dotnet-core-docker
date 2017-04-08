e3db86e0e519092e00b9dd05166beb52a3469698
Jack Histon <jrhiston@gmail.com>
2017-04-08 14:37:34 +0100

    Updates

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -14,7 +14,6 @@ namespace app
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
